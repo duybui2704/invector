521bd2af0d4e7cc8513729afaebd5e226561430c
Quy <quypm@tienngay.vn>
2022-06-14 11:54:02 +0700

    fix

--- src/common/Languages.ts
+++ src/common/Languages.ts
@@ -1,7 +1,7 @@
 const Languages = {
     board: {
         title1: 'Đầu tư an toàn cùng Tienngay.vn',
-        txt1: 'TienNgay.vn có giấy phép kinh doanh và hoạt động theo quy định của pháp luật và đồng nhất trên toàn hệ thống. Số tiền lãi được trả đúng hạn theo cam kết, giải quyết thắc mắc (nếu có) tận tình, nhanh chóng',
+        txt1: 'TienNgay.vn có giấy phép kinh doanh và hoạt động theo quy định của pháp luật và đồng nhất trên toàn hệ thống. Số tiền lãi được trả đúng hạn theo cam kết, giải quyết thắc mắc (nếu có) tận tình, nhanh chóng.',
         title2: 'Lãi suất hấp dẫn cùng Tienngay ',
         txt2: 'Tham gia gửi tiền có kỳ hạn Online tại Tienngay để có thêm thời gian tận hưởng cuộc sống và luôn an tâm rằng tiền của bạn đang sinh lời với lãi suất hấp dẫn.',
         title3: 'Rõ ràng - Minh bạch',
