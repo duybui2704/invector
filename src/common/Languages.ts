1ff5d151cf66946ac040209a3f0166727edfff40
duybx <duybx@tienngay.vn>
2022-07-07 13:04:00 +0700

    fix name TienNgay.vn

--- src/common/Languages.ts
+++ src/common/Languages.ts
@@ -2,8 +2,8 @@ const Languages = {
     board: {
         title1: 'Đầu tư an toàn cùng TienNgay.vn',
         txt1: 'TienNgay.vn có giấy phép kinh doanh và hoạt động theo quy định của pháp luật và đồng nhất trên toàn hệ thống. Số tiền lãi được trả đúng hạn theo cam kết, giải quyết thắc mắc (nếu có) tận tình, nhanh chóng.',
-        title2: 'Lãi suất hấp dẫn cùng TienNgay ',
-        txt2: 'Tham gia gửi tiền có kỳ hạn Online tại TienNgay để có thêm thời gian tận hưởng cuộc sống và luôn an tâm rằng tiền của bạn đang sinh lời với lãi suất hấp dẫn.',
+        title2: 'Lãi suất hấp dẫn cùng TienNgay.vn ',
+        txt2: 'Tham gia gửi tiền có kỳ hạn Online tại TienNgay.vn để có thêm thời gian tận hưởng cuộc sống và luôn an tâm rằng tiền của bạn đang sinh lời với lãi suất hấp dẫn.',
         title3: 'Rõ ràng - Minh bạch',
         txt3: 'Tất cả các thao tác của Nhà đầu tư được ghi lại bằng hệ thống của TienNgay.vn, đảm bảo số tiền lãi nhận về rõ ràng, minh bạch.'
     },
@@ -155,11 +155,11 @@ const Languages = {
         skip: 'Bỏ qua'
     },
     introduce: {
-        txtIntroduce1: 'Đầu tư an toàn cùng TienNgay',
+        txtIntroduce1: 'Đầu tư an toàn cùng TienNgay.vn',
         txtIntroduce2: 'TienNgay.vn có giấy phép kinh doanh và hoạt động theo quy định của pháp luật và đồng nhất trên toàn hệ thống. Số tiền lãi được trả đúng hạn theo cam kết, giải quyết thắc mắc (nếu có) tận tình, nhanh chóng',
         txtContinue: 'Tiếp tục',
-        txtIntroduce3: 'Lãi suất hấp dẫn cùng TienNgay',
-        txtIntroduce4: 'Tham gia gửi tiền có kỳ hạn Online tại TienNgay để có thêm thời gian tận hưởng cuộc sống và luôn an tâm rằng tiền của bạn đang sinh lời với lãi suất hấp dẫn.',
+        txtIntroduce3: 'Lãi suất hấp dẫn cùng TienNgay.vn',
+        txtIntroduce4: 'Tham gia gửi tiền có kỳ hạn Online tại TienNgay.vn để có thêm thời gian tận hưởng cuộc sống và luôn an tâm rằng tiền của bạn đang sinh lời với lãi suất hấp dẫn.',
         introduce5: 'Rõ ràng - Minh bạch',
         introduce6: 'Tất cả các thao tác của Nhà đầu tư được ghi lại bằng hệ thống của TienNgay.vn, đảm bảo số tiền lãi nhận về rõ ràng, minh bạch.'
     },
